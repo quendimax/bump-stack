075a2b92d09c400b76e32aac34f26bdb53aa5b4f
Maksim Tomkavich <quendimax@gmail.com>
2025-10-29 20:12:00 +0100

    test: add some tests

--- src/utest.rs
+++ src/utest.rs
@@ -63,19 +63,18 @@ fn stack_one_chunk() {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+struct Element {
+    _value: [usize; 6],
+}
+
+fn elem(i: usize) -> Element {
+    Element { _value: [i; 6] }
+}
+
 #[test]
-fn stack_two_chunk() {
-    #[derive(Debug, PartialEq, Eq)]
-    struct Element {
-        _value: [usize; 6],
-    }
-
-    fn elem(i: usize) -> Element {
-        Element { _value: [i; 6] }
-    }
-
-    type Stack = super::Stack<Element>;
-    let mut stack = Stack::new();
+fn stack_two_chunks() {
+    let mut stack = Stack::<Element>::new();
 
     stack.push(elem(0));
     let capacity = stack.capacity();
@@ -133,5 +132,94 @@ fn stack_two_chunk() {
         assert!(!current_footer.is_dead());
         assert!(current_footer.prev.get().as_ref().is_dead());
         assert!(current_footer.next.get().as_ref().is_dead());
+    }
+}
+
+#[test]
+fn stack_three_chunks() {
+    let mut stack = Stack::<Element>::new();
+
+    stack.push(elem(0));
+    let capacity_1 = stack.capacity();
+
+    for i in 1..capacity_1 + 1 {
+        stack.push(elem(i));
+    }
+
+    let capacity_12 = stack.capacity();
+
+    for i in capacity_1 + 1..capacity_12 + 1 {
+        stack.push(elem(i));
+    }
+
+    let capacity_123 = stack.capacity();
+    assert!(capacity_12 < capacity_123);
+
+    for i in capacity_12 + 1..capacity_123 {
+        assert_eq!(stack.len(), i);
+        stack.push(elem(i));
+        assert_eq!(stack.capacity(), capacity_123);
+    }
+
+    assert_eq!(stack.len(), stack.capacity());
+    assert_eq!(stack.capacity(), capacity_123);
+
+    unsafe {
+        let current_footer = stack.current_footer.as_ref();
+        assert!(!current_footer.is_dead());
+        assert!(!current_footer.prev.get().as_ref().is_dead());
+        assert!(current_footer.next.get().as_ref().is_dead());
+    }
+
+    for i in (capacity_12..capacity_123).rev() {
+        assert_eq!(stack.pop(), Some(elem(i)));
+    }
+
+    assert_eq!(stack.len(), capacity_12);
+    assert_eq!(stack.capacity(), capacity_123);
+
+    unsafe {
+        let current_footer = stack.current_footer.as_ref();
+        assert!(!current_footer.is_dead());
+        assert!(current_footer.is_empty());
+        assert!(!current_footer.prev.get().as_ref().is_dead());
+        assert!(current_footer.next.get().as_ref().is_dead());
+    }
+
+    for i in (capacity_1..capacity_12).rev() {
+        assert_eq!(stack.pop(), Some(elem(i)));
+    }
+
+    assert_eq!(stack.len(), capacity_1);
+    assert_eq!(stack.capacity(), capacity_123);
+
+    unsafe {
+        let current_footer = stack.current_footer.as_ref();
+        assert!(!current_footer.is_dead());
+        assert!(current_footer.is_empty());
+        assert!(!current_footer.prev.get().as_ref().is_dead());
+        assert!(!current_footer.next.get().as_ref().is_dead());
+        assert!(current_footer.next.get().as_ref().is_empty());
+    }
+
+    for i in (0..capacity_1).rev() {
+        assert_eq!(stack.pop(), Some(elem(i)));
+    }
+
+    let capacity_13 = capacity_123 - capacity_12 + capacity_1;
+
+    assert_eq!(stack.capacity(), capacity_13);
+    assert_eq!(stack.len(), 0);
+
+    unsafe {
+        let current_footer = stack.current_footer.as_ref();
+        assert!(!current_footer.is_dead());
+        assert!(current_footer.is_empty());
+        assert!(current_footer.prev.get().as_ref().is_dead());
+        let next_footer = current_footer.next.get().as_ref();
+        assert!(!next_footer.is_dead());
+        assert!(next_footer.is_empty());
+        assert_eq!(next_footer.prev.get(), stack.current_footer);
+        assert!(next_footer.next.get().as_ref().is_dead());
     }
 }
---
+++ tests/itest.rs
@@ -0,0 +1,27 @@
+use bump_stack::Stack;
+
+#[test]
+fn stack_ctor() {
+    let s = Stack::<i32>::new();
+    assert_eq!(s.capacity(), 0);
+    assert_eq!(s.len(), 0);
+
+    let s = Stack::<i32>::default();
+    assert_eq!(s.capacity(), 0);
+    assert_eq!(s.len(), 0);
+}
+
+#[test]
+fn stack_push_pop() {
+    let mut stack = Stack::<usize>::new();
+    const MAX: usize = 1 << 16;
+    for i in 0..MAX {
+        assert_eq!(stack.len(), i);
+        stack.push(i);
+    }
+    for i in (0..MAX).rev() {
+        assert_eq!(stack.pop(), Some(i));
+    }
+    assert_eq!(stack.len(), 0);
+    assert!(stack.len() < stack.capacity());
+}
